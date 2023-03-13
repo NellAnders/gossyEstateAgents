fc3a8b628005caf1e9a42b12ba503b34c2321b4b
adeembruce <109042426+adeembruce@users.noreply.github.com>
2023-03-13 15:44:30 +0000

    Add files via upload
    initial mockups

---
+++ gossyEstateAgent/table.php
@@ -0,0 +1,52 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="styleTable.css">
+    
+</head >
+    <body>
+    
+    
+    
+    <div class="container">
+
+      <div class="griditem1">
+        <h1></h1>
+
+
+        <div class="home" ><a href="homePage.php">Home</a></div>
+
+        <div class="signedIn" ><label class="signedIn label">Signed in as: </label></div>
+
+        <div class="signOut" ><a href="page2.html">Sign out</a></div>
+
+      </div>
+
+      <div class="grid-item item2">
+
+        <div class="grid-container">
+            <div class="grid-item item1">
+            <button type="button">Add new</button>
+            <label>Table Title</label>
+            <button type="button">Search</button>
+            </div>
+
+            <div class="grid-item item2">Column Tiles</div>
+            <div class="grid-item item3"></div>  
+            <div class="grid-item item4"></div>
+            <div class="grid-item item5"></div>
+            <div class="grid-item item6"></div>  
+            <div class="grid-item item7"></div>
+            <div class="grid-item item8"></div>
+        </div>
+        
+    </div>
+    
+    </body>
+</html>
